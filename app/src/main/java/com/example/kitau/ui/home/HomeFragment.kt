4a5b8a12cddfe75328a457ab348265b15e52c777
angio <honorevoleferretti@live.it>
2021-04-28 19:27:39 +0200

    ui quasi pronta

--- app/src/main/java/com/example/kitau/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kitau/ui/home/HomeFragment.kt
@@ -18,10 +18,6 @@ class HomeFragment : Fragment() {
 
     private lateinit var auth: FirebaseAuth
 
-
-
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
